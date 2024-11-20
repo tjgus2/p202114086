0ddb6888486480b1b3c411f4dfd45a48090a15fc
Park, Seohyun <tjgus6279@naver.com>
2024-11-20 09:48:49 +0900

    four

--- src/p202114086/Main.java
+++ src/p202114086/Main.java
@@ -4,5 +4,12 @@ public class Main {
 	  public static void main(String[] args) {
 	    //  자신의 학번과 이름을 출력하라
 	    System.out.println("202114086 박서현");
+	    System.out.println(Hello.sum(10));
+	    System.out.println(Hello.sum(100));
+	    System.out.println(Hello.sum(1000));
+	    System.out.println(World.factorial(3));
+	    System.out.println(World.factorial(6));
+	    System.out.println(World.factorial(9));
+
 	  }
 	}
---
+++ src/p202114086/World.java
@@ -0,0 +1,12 @@
+package p202114086;
+
+public class World {
+
+	  public static long factorial(int n) {
+	    long result = 1;
+	    for (int i = 1; i <= n; ++i)
+	      result *= i;
+	    return result;
+	  }
+	}
+
