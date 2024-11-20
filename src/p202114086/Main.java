2058d60a9c05a3d42a1f29b9968845916e347ca0
Park, Seohyun <tjgus6279@naver.com>
2024-11-20 09:51:35 +0900

    five

--- src/p202114086/Main.java
+++ src/p202114086/Main.java
@@ -6,6 +6,10 @@ public class Main {
 	    System.out.println("202114086 박서현");
 		System.out.println(Hello.sum(10));
 		System.out.println(Hello.sum(100));
+		System.out.println(Hello.sum(1000));
+		System.out.println(Sansu.max(new int[] {8, 5, 6, 9, 4, 1, 7, 10, 3, 2, 8}));
+		System.out.println(Sansu.max(new int[] {-2, 5, 6, 9, -6, 1, 7, 2, 8, 10}));
+	
 	
 	  }
 	}
